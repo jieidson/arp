e76d14111a20019de5a4195e37c30da1c3bce030
Tom Wambold <tom5760@gmail.com>
2018-11-27 15:57:22 -0500

    Refactor simulation loop to be more explicit about phases.

--- sim/simulator.go
+++ sim/simulator.go
@@ -51,7 +51,7 @@ func (s *Simulator) Loop() error {
 	percent := 0
 
 	for s.CurrentTick = 0; s.CurrentTick < totalTicks; s.CurrentTick++ {
-		if err := s.Tick(); err != nil {
+		if err := s.tick(); err != nil {
 			return fmt.Errorf("failed to run tick: %v", err)
 		}
 
@@ -64,38 +64,17 @@ func (s *Simulator) Loop() error {
 	return nil
 }
 
-// Tick executes a single tick of the simulation.
-func (s *Simulator) Tick() error {
-	for _, agent := range s.Agents {
-		agent.Move(s.Provider)
+// tick executes a single tick of the simulation.
+func (s *Simulator) tick() error {
+	s.movementPhase()
+	s.actionPhase()
+
+	if err := s.logAgents(); err != nil {
+		return err
 	}
 
-	for _, agent := range s.Agents {
-		agent.Action(s.Provider)
-	}
-
-	agentDataWriter := s.Provider.AgentDataWriter()
-	var agentDataRow AgentDataRow
-	agentDataRow.Timestep = s.CurrentTick
-
-	for _, agent := range s.Agents {
-		agent.Log(s.Provider, &agentDataRow)
-
-		if err := agentDataRow.Write(agentDataWriter); err != nil {
-			return fmt.Errorf("failed to write agent data: %v", err)
-		}
-	}
-
-	nodeDataWriter := s.Provider.NodeDataWriter()
-	var nodeDataRow NodeDataRow
-	nodeDataRow.Timestep = s.CurrentTick
-
-	for _, node := range s.Provider.Arena().Nodes {
-		node.Log(s.Provider, &nodeDataRow)
-
-		if err := nodeDataRow.Write(nodeDataWriter); err != nil {
-			return fmt.Errorf("failed to write node data: %v", err)
-		}
+	if err := s.logNodes(); err != nil {
+		return err
 	}
 
 	return nil
@@ -117,4 +96,50 @@ func (s *Simulator) generateOffenders(c config.Config) {
 	for i := uint64(0); i < c.Agent.Offender; i++ {
 		s.Agents = append(s.Agents, NewPoliceAgent(uint64(len(s.Agents))))
 	}
+}
+
+func (s *Simulator) movementPhase() {
+	for _, agent := range s.Agents {
+		agent.Move(s.Provider)
+	}
+}
+
+func (s *Simulator) actionPhase() {
+	for _, agent := range s.Agents {
+		agent.Action(s.Provider)
+	}
+}
+
+func (s *Simulator) logAgents() error {
+	writer := s.Provider.AgentDataWriter()
+
+	var row AgentDataRow
+	row.Timestep = s.CurrentTick
+
+	for _, agent := range s.Agents {
+		agent.Log(s.Provider, &row)
+
+		if err := row.Write(writer); err != nil {
+			return fmt.Errorf("failed to write agent data: %v", err)
+		}
+	}
+
+	return nil
+}
+
+func (s *Simulator) logNodes() error {
+	writer := s.Provider.NodeDataWriter()
+
+	var row NodeDataRow
+	row.Timestep = s.CurrentTick
+
+	for _, node := range s.Provider.Arena().Nodes {
+		node.Log(s.Provider, &row)
+
+		if err := row.Write(writer); err != nil {
+			return fmt.Errorf("failed to write node data: %v", err)
+		}
+	}
+
+	return nil
 }
